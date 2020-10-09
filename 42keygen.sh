7fc1522fa979651e8c269be6abd965e187d2b2a6
Seo Jaehyeon <sjaehyeo@c1r2s7.42seoul.kr>
2020-10-09 22:06:47 +0900

    wow

--- 42keygen.sh
+++ 42keygen.sh
@@ -9,4 +9,4 @@ rm -rf ~/.ssh/known_hosts
 ssh-keygen -t rsa -b 4096 -C $id@student.42seoul.kr
 eval "$(ssh-agent -s)"
 ssh-add -K ~/.ssh/id_rsa
-pbcopy < ~/.ssh/id_rsa.pubs
+pbcopy < ~/.ssh/id_rsa.pub
