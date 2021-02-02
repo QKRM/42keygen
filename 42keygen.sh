aa933b19ccda12b8c961856cd3d75b419312e1c2
QKRM <48343739+QKRM@users.noreply.github.com>
2021-02-02 19:20:23 +0900

    Update 42keygen.sh

--- 42keygen.sh
+++ 42keygen.sh
@@ -10,3 +10,12 @@ ssh-keygen -t rsa -b 4096 -C $id@student.42seoul.kr
 eval "$(ssh-agent -s)"
 ssh-add -K ~/.ssh/id_rsa
 pbcopy < ~/.ssh/id_rsa.pub
+
+while getopts "s:" opt
+do 
+	case $opt in
+		s)
+			cat ~/.ssh/id_rsa.pub
+			;;
+	esac
+done
